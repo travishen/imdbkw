dc0de1502cbdda09d7544ba9aec633cf1e6e9d92
ssivart <travishen.tw@gmail.com>
2017-10-24 19:23:59 +0800

    Add files via upload

--- imdbkw/imdbkw.py
+++ imdbkw/imdbkw.py
@@ -82,7 +82,7 @@ class Keyword(Base):
 
 def setup_engine(dburl):
     global engine, Session
-    engine = create_engine(dburl, pool_size=20 , max_overflow=-1, pool_recycle=1200)
+    engine = create_engine(dburl, pool_size=50 , max_overflow=-1, pool_recycle=1200)
     session_factory = sessionmaker(bind=engine, autoflush=False)
     Session = scoped_session(session_factory)
 
